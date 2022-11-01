cc47512710d58c137e22565a8620b73117246733
dmstepanuik <0669218646@ukr.net>
2022-11-01 21:41:58 +0200

    task 03-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,26 @@ import SearchForm from './SearchForm/SearchForm';
 import ContactList from './ContactList/ContactList';
 import s from './App.module.css';
 
+const LOCAL_KEY = 'Task03/contacts';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts =
+      JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? this.state.contacts;
+
+    this.setState(() => ({ contacts }));
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
+  }
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts === prevState.contacts) return;
+
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(this.state.contacts));
+  }
 
   onChangeFilter = value => {
     this.setState({
